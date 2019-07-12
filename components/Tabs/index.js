8a7d322d867dcf3156a01cf9bf8944f9c6db19a0
Chris Johnston <cmjohnston@gmail.com>
2019-07-12 10:59:56 -0500

    create tab component and update README checklist

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,19 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(data => {
+        const topics = data.data.topics;
+        topics.forEach(topic => {
+            const topicDiv = document.createElement('div');
+            const topicClass = document.querySelector('.topics');
+            topicDiv.classList.add('tab');
+            topicDiv.textContent = topic;
+            topicClass.appendChild(topicDiv)
+        })
+        console.log("It works", topics);
+    })
+    .catch(error => {
+        console.log("Sorry, something went wrong. A team of highly trained monkeys has been dispatched to deal with this situation", error);
+    })
