f207227f7f75a61a2b4b02d7cb5e18ec3089e1a8
Kelly M <moreira.kd@outlook.com>
2019-07-12 12:40:49 -0400

    card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function Cards () {
+
+    // new elements
+
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const authorPic = document.createElement('div');
+    const divImg = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');
+
+
+    // structure 
+
+    card.appendChild(headline);
+    card.appendChild(authorPic);
+    card.appendChild(divImg);
+    divImg.appendChild(img);
+    card.appendChild(authorName);
+
+
+    // class names
+
+    card.classList.add('card');
+    card.classList.add('headline');
+    card.classList.add('author');
+    card.classList.add('img-container');
+
+
+    // text content
+
+    headline.textContent = '{Headline of article}';
+    authorName.textContent = 'By {authors name}';
+
+
+
+    return card;
+
+
+
+
+
+
+}
