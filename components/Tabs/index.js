2bfd00c57a811dc5aa961889524eca568dc8829d
Suparada Saitalae <ann.suparada@gmail.com>
2019-07-12 11:17:46 -0500

    using axios

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,11 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+const topicsEle = document.querySelector('.topics')
+
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
+.then(data => {
+    console.log('yay',data)
+})
