04d36f4eeaff3e3a4b87b6d5c3b852321cc3219a
Suparada Saitalae <ann.suparada@gmail.com>
2019-07-12 12:05:42 -0500

    using .axios, .then, .catch

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,11 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+.then(data => {
+    console.log('it is working', data)
+})
+.catch(error => {
+    console.log('The Article API is currently down, try again later', error)
+})
