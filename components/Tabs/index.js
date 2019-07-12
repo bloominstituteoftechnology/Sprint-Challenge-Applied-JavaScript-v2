0581fc0a900553665dfcf2c5318dcd1caff69c55
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-07-12 11:13:03 -0500

    working on tabs js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,9 +1,26 @@
 // Step 2: Create Tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
+
+//data: {"topics":["javascript","bootstrap","technology","jquery","node.js"]}
+
+
 // Once the data is returned console.log it and review the structure.
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// const topicsHtml = document.querySelector('.topics');
+//want to add the tabCreator(arrayParam) to topicsHtml later [...]
+
+
+
+
+//tab component
+// const tabCreator = (arrayParam) => {
+
+// }
+
+//testing... 
