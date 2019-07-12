5b822568c840cf00314dffb0d407fa572c572597
Davindar <davdavdav4@gmail.com>
2019-07-12 18:27:18 +0100

    header

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,37 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the header-container component
 
-function Header() {}
+function Header() {
+
+//define new elements
+const head = document.createElement('div');
+const date = document.createElement('span');
+const headingOne = document.createElement('h1');
+const temperature = document.createElement('span');
+
+//setup structure of elements
+head.appendChild(date)
+head.appendChild(headingOne)
+head.appendChild(temperature)
+
+//set class names
+head.classList.add('header');
+date.classList.add('date');
+headingOne.classList.add('h1');
+temperature.classList.add('temp');
+
+//set text content
+date.textContent = "button";
+headingOne.textContent = "Lambda Times";
+temperature.textContent = "95%";
+
+
+return head;
+}
+
+const project = document.querySelector('.header-container');
+console.log(project)
+
+project.appendChild(Header());
