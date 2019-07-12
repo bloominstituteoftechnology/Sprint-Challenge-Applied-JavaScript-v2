4dc44d967811e8640d0e20294cb50ef6948aab52
Chris Johnston <cmjohnston@gmail.com>
2019-07-12 11:07:47 -0500

    working on card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,37 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data => {
+        const javaArticles = data.data.articles.javascript
+        const bootStrapArticles = data.data.articles.bootstrap;
+        const techArticles = data.data.articles.technology;
+        const jqueryArticles = data.data.articles.jquery;
+        const nodeArticles = data.data.articles.node
+
+        const articles = [jsArticles, bootStrapArticles, techArticles, jqueryArticles, nodeArticles]
+
+        articles.forEach(article => {
+            article.forEach(art => {
+                const cards = document.querySelector('.cards-container');
+                cards.appendChild(Article(art));
+            })
+        })
+        console.log('Success', data);
+    })
+    .catch(error => {
+        console.log("ERROR", error);
+    })
+
+function Article(obj) {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    return card;
+
+}
