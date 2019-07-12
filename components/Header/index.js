c82e23ee0457bae1da576c817177dc8407ce7387
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-07-12 10:50:04 -0500

    finished header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,34 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const headerContainerHtml = document.querySelector('.header-container');
+
+
+function Header() {
+    //create elements
+    headerContainer = document.createElement('div');
+    spanC1 = document.createElement('span');
+    h1C = document.createElement('h1');
+    spanC2 = document.createElement('span');
+
+    //classnames 
+    headerContainer.classList.add('header');
+    spanC1.classList.add('date');
+    spanC2.classList.add('temp');
+
+    //append elements
+    // headerContainerHtml.appendChild(headerContainer);
+    headerContainer.appendChild(spanC1);
+    headerContainer.appendChild(h1C);
+    headerContainer.appendChild(spanC2);
+
+    //content
+    spanC1.textContent = `SMARCH 28, 2019`;
+    h1C.textContent = `Lambda Times`;
+    spanC2.textContent = `98°`;
+
+    //return
+    return headerContainer;
+}
+
+headerContainerHtml.appendChild(Header());
