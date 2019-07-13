453bc772dfbbf41ee6d11658d562a90a0dc7c15c
juarezfrench <markedfrench@gmail.com>
2019-07-13 17:19:28 -0500

    Built, feeding data thgouhg, just working on iteration.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+insertCards = document.querySelector(".cards-container");
+
+axios
+  .get(`https://lambda-times-backend.herokuapp.com/articles`)
+  .then(data => {
+    const articleData = Object.entries(data.data.articles);
+    const categories = Object.keys(data.data.articles);
+
+    console.log("index.js -> %carticleData:", "color: blue", articleData);
+    console.log("index.js -> %ccategories:", "color: green", categories);
+
+    categories.forEach(cat => {
+      articleData.forEach(article => {
+        if (articleData[0][0] == cat) {
+          
+          
+          (makeCards(article));
+        }
+      });
+    });
+  })
+
+  .catch(error => {
+    console.log("index.js -> %c\"There's a data issue:", "color: red", error);
+  });
+
+function makeCards(data) {
+  const cards = document.createElement("div");
+  const headLn = document.createElement("div");
+  const author = document.createElement("div");
+  const imgCont = document.createElement("div");
+  const authImg = document.createElement("img");
+
+  insertCards.appendChild(cards);
+  cards.appendChild(headLn);
+  cards.appendChild(author);
+  author.appendChild(imgCont);
+  author.appendChild(authImg);
+
+  cards.classList.add("card");
+  headLn.classList.add("headline");
+  author.classList.add("author");
+  imgCont.classList.add("img-container");
+
+  headLn.textContent = data[1][0].headline;
+  authImg.src = data[1][0].authorPhoto; 
+  author.textContent = data[1][0].authorName;
+  console.log("index.js -> %cdata:", "color: red", data);
+}
+//[""0""][1][""0""]
--- components/Header/index.js
+++ components/Header/index.js
@@ -3,10 +3,44 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
 function Header() {}
+
+const insertionPoint = document.querySelector(".header-container");
+//console.log(
+//   'index.js -> %cdocument.querySelector(".header-container"):',
+//   "color: #baba09",
+//   document.querySelector(".header-container")
+// );
+
+makeHeader();
+
+function makeHeader(data) {
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const title = document.createElement("h1");
+  const temperature = document.createElement("span");
+
+  // Setup structure of elementsreturn artBox;
+
+  insertionPoint.appendChild(header);
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(temperature);
+
+  // set class names
+  header.classList.add("header");
+  date.classList.add("date");
+  temperature.classList.add("temp");
+
+  ///// content /////
+
+  date.textContent = "MARCH 28, 2019";
+  title.textContent = "Lambda Times";
+  temperature.textContent = "98°";
+}
